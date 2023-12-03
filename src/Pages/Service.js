b2bd76b8207b31f9a30edc66019b21126908f28a
Mindless Madness <professor.windu@gmail.com>
2023-12-03 19:10:30 +0530

    edits

--- src/Pages/Service.js
+++ src/Pages/Service.js
@@ -8,15 +8,16 @@ import { useState } from "react";
 export default function Service(){
     const location = useLocation();
     const {loading, error, data} = useFetch(`http://localhost:8085/service/${parseInt(location.pathname.split("/")[3])}`);
-    const [showRateScreen, setShowRateScreen] = useState(false);
+    const [pos, setPos] = useState({x:0, y:0});
     const [showAddToCollectionScreen, setShowAddToCollectionScreen] = useState(false);
     console.log(data)
     if(loading) return <Loading/>
     if(error) return <Error/>
     if(data.error) return <Error/>
-    if(showRateScreen) return <RateScreen setShowRateScreen={setShowRateScreen}/>
-    if(showAddToCollectionScreen) return <AddToCollectionScreen setShowAddToCollectionScreen={setShowAddToCollectionScreen}/>
+    // if(showRateScreen) return <RateScreen setShowRateScreen={setShowRateScreen}/>
+    // if(showAddToCollectionScreen) return <AddToCollectionScreen setShowAddToCollectionScreen={setShowAddToCollectionScreen}/>
     return <div className="serviceWrap">
+        {showAddToCollectionScreen && <AddToCollectionScreen pos={pos} setShowAddToCollectionScreen={setShowAddToCollectionScreen}/>}
         <div className="serviceUp">
         <img className="serviceImage" alt="img" src={data.service.image}/>
         <div className="details">
@@ -36,10 +37,10 @@ export default function Service(){
             <a target="_blank" href={`tel:${data.service.phno}`} className="contactInfo on-surface-text body-large primary-text" rel="noreferrer">{data.service.phno+" "}</a>
             <div className="on-surface-text material-symbols-rounded fill-icon icon contactIcon">mail </div>
             <a target="_blank" href={`mailto:${data.service.email}`} className="contactInfo on-surface-text body-large primary-text" rel="noreferrer">{data.service.email}</a>
-            <div className="on-surface-text material-symbols-rounded fill-icon icon contactIcon">star </div>
-            <div onClick={()=>{setShowRateScreen(true)}} className="pointer contactInfo on-surface-text body-large primary-text">Click to Rate </div>
+            {/* <div className="on-surface-text material-symbols-rounded fill-icon icon contactIcon">star </div>
+            <div onClick={()=>{setShowRateScreen(true)}} className="pointer contactInfo on-surface-text body-large primary-text">Click to Rate </div> */}
             <div className="on-surface-text material-symbols-rounded fill-icon icon contactIcon">add </div>
-            <div onClick={()=>{setShowAddToCollectionScreen(true)}} className="contactInfo pointer on-surface-text body-large primary-text">Add To Collection</div>
+            <div onClick={(event)=>{setPos({x:event.clientX, y:event.clientY});setShowAddToCollectionScreen(!showAddToCollectionScreen);}} className="contactInfo pointer on-surface-text body-large primary-text">Add To Collection</div>
             </div>
         </div>
         </div>
@@ -49,6 +50,7 @@ export default function Service(){
             {data.service.reviews.map((item, index)=>{
                 return <ReviewItem key={index} review={item}></ReviewItem>
             })}
+            <RateScreen/>
         </div>
         <div className="additional">
         <p className="on-surface-text headline-medium">Additional Information</p> 
@@ -57,6 +59,7 @@ export default function Service(){
         lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid id.
         lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid id.</p>
         </div>
+       
        </div>
     </div>
 }
@@ -76,12 +79,12 @@ function ReviewItem({review}){
     </div>
 }
 
-function RateScreen({setShowRateScreen}){
+function RateScreen(){
     return <div className="rateScreen">
-    <div onClick={()=>{setShowRateScreen(false)}}><span className="header-large primary-text filter material-symbols-rounded">close</span></div>
+    {/* <div onClick={()=>{setShowRateScreen(false)}}><span className="header-large primary-text filter material-symbols-rounded">close</span></div> */}
     
     <div className="rating-form">
-        <div className="df"><span style={{marginTop: "10px"}} className="on-surface-text title-large">Click to rate: </span><ReactStars
+        <div className="df"><p style={{marginTop: "10px"}} className="on-surface-text headline-medium">Rate </p></div><ReactStars
                 count={5}
                 // isHalf={true}
                 size={32}
@@ -89,17 +92,19 @@ function RateScreen({setShowRateScreen}){
                 // edit={false}
                 // className="on-surface-text headline-large primary-text"
                 
-            /></div>
-        <textarea style={{marginTop: "10px"}}>Write a Review....</textarea>
-        <div onClick={()=>{setShowRateScreen(false)}}  style={{marginTop: "10px"}} className="al-sl-rt on-surface-text material-symbols-rounded fill-icon icon contactIcon">send</div>
+            />
+        <textarea placeholder="Write a Review...." style={{marginTop: "10px"}}></textarea>
+        <div style={{marginTop: "10px"}} className="al-sl-rt on-surface-text material-symbols-rounded fill-icon icon contactIcon">send</div>
     </div>
 </div>
 }
 
-function AddToCollectionScreen({setShowAddToCollectionScreen}){
-    return <><span onClick={()=>{setShowAddToCollectionScreen(false)}} className="header-large primary-text filter material-symbols-rounded">close</span><div className="addtocollScreen">
+function AddToCollectionScreen({setShowAddToCollectionScreen, pos}){
+    console.log(pos)
+    return <><div style={{top:`${pos.y}px`, left:`${pos.x}px`}} className="surface addtocollScreen">
+        
     <div className=" addtocollform" >
-    <span style={{marginTop: "10px"}} className="on-surface-text title-large">Add to Collection: </span>
+    <span style={{marginTop: "10px"}} className="on-surface-text title-large">Add to Collection </span><span className="on-surface-text material-symbols-rounded"> close</span>
     <div className="yscroll">
         {
             [0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5].map((item, index)=>{
