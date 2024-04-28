a00b3d0425ae20ebdd54362d2f71b7558001c89d
Mindless Madness <professor.windu@gmail.com>
2024-04-28 15:24:44 +0530

    ci test

--- test.mjs
+++ test.mjs
@@ -3,12 +3,13 @@ import {listner} from "./server.js"
 import { after } from 'mocha';
 describe("Tests", ()=>{
     it("should pass", ()=>{
-        assert.strictEqual(0, 0, "Sample test");
+        assert.strictEqual("working", "working", "Sample test");
     })
 
     it("should have NODE_ENV==test", ()=>{
         assert.strictEqual(process.env.NODE_ENV, "test");
     })
+
     after(()=>{
         console.log("Closing server");
         listner.close();
